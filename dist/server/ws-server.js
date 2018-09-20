426345c579fd0044061ddb9228ec86a46f88a728
Pablo <pablo.diaz.callejo@gmail.com>
2018-09-20 10:02:59 +0200

    arreglos en el servidor

--- dist/server/ws-server.js
+++ dist/server/ws-server.js
@@ -16,17 +16,29 @@ const wss = new WebSocket.Server({ server }); // Server(): el parametro server e
 // que sirve de soporte de comunicacion
 // El servidor WebSocket escucha algunos eventos...
 wss.on('connection', (ws) => {
+    // Datos a transmitir a los clientes
+    let envelop = {}; // object any = puedo hacer lo que quiera con el
     // La conexion esta bien, enviamos un mensaje de que connexion ok
     ws.on('message', (message) => {
         // Muestra el mensaje en la consola y vuelve al cliente
-        console.log('Recibido: %s', message);
-        ws.send(`Hola, usted viene de enviar -> ${message}`);
+        console.log('Recibido: %s [%d]', message, new Date()); // %s = string (lo remplaza por message que es variable)
+        envelop.message = ' Su mensaje : ' + message + ' ha sido enviado '; // concepto de "eco local"
+        // Eco para el emisor...
+        ws.send(JSON.stringify(envelop)); //metodo send de ws:
+        // Broadcast hacia los otros clientes --> Difundir el mensaje hacia todos los clientes
+        wss.clients
+            .forEach(client => {
+            if (client != ws) {
+                envelop.message = 'Un nuevo mensaje ha sido transmitido :' + message;
+                client.send(JSON.stringify(envelop));
+            }
+        });
     });
     //Envia inmediatamente una informacion al cliente conectado
-    ws.send('Hola, soy el servidor WebSocket');
+    ws.send({ message: 'Hola cliente!! Bienvenido al servidor WebSocket de "ChatApp"' });
 });
 // Lanzamos el servidor
 server.listen(process.env.PORT || 8999, () => {
-    console.log('El servidor ha sido iniciado y escucha en la direccion:' + server.address());
+    console.log('El servidor ha sido iniciado y escucha en la direccion:' + server.address()); // OJO AQUI!!! 
 });
 //# sourceMappingURL=ws-server.js.map
